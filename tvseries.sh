9092b567c4235141c512d67b48950b9b00d1c565
Jonathan Diehl <jonathan.diehl@gmail.com>
2013-01-22 14:47:14 +0100

    updated tvseries import

--- tvseries.sh
+++ tvseries.sh
@@ -4,18 +4,18 @@ function encode {
 	-i "$1" \
 	-o "$2" \
 	-t$3 \
-	-a1,2 \
+	-a1,3 \
 	-m \
 	--loose-anamorphic \
 	-ex264 \
-	--x264-tune=animation \
-	-q21.0 \
+	--x264-tune=film \
+	-q20.0 \
 	-r30 \
 	--pfr \
-	-x b-adapt=2 \
+	-x b-adapt=2:psnr=0:ssim=0 \
 	-X1920 \
 	-Eca_aac,ca_aac \
-	-dslower
+	-5
 }
 
 function getDVDPath {
@@ -29,7 +29,7 @@ do
 	getDVDPath
 done
 
-output=/Users/jon/Movies/
+output=/Users/jon/Movies/TV\ Shows/
 title=$1
 episode_offset=$3
 season=$2
@@ -38,8 +38,9 @@ season=$2
 for ((i=1; i<=$4; i++))
 do
 	episode=$(($episode_offset + $i - 1))
+	track=$(($i))
 	mkdir -p "$output$title/Season $season/"
-	encode $input "$output$title/Season $season/$title.S$season.E$episode.mp4" $i
+	encode $input "$output$title/Season $season/$title.S$season.E$episode.mp4" $track
 done
 
 # eject disk
